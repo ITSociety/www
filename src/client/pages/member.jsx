edd816f368fe115d6c3f381dbae7eb428b15f470
Harry <hfitz83@gmail.com>
2017-11-12 21:09:45 +0000

    todo: finish member.jsx

--- src/client/pages/member.jsx
+++ src/client/pages/member.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Loading } from '../partial';
 import { getEndpoint, markdownToReact } from '../util.jsx';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import Card, { CardContent, CardMedia } from 'material-ui/Card';
+import Icon from 'material-ui/Icon';
 
 const generatePage = member => {
-
+  <div className='member'>
+    
 };
 
 
@@ -18,6 +23,16 @@ export default class MemberPage extends Component {
     const url = `/api/contentful/committee/${this.id}`;
     const memberInfo = await getEndpoint(url);
     const children = generatePage(memberInfo);
-    this.setState({ children });
+    // this.setState({ children });
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <Grid container spacing={40} alignItems="stretch" justify="space-around" className="gutter">
+          {this.state.children}
+        </Grid>
+      </div>
+    );
   }
 }
--- src/client/router.jsx
+++ src/client/router.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Layout from './layout.jsx';
-import { Home, EventPage, AboutPage, PastEventsPage } from './pages';
+import { Home, EventPage, AboutPage, PastEventsPage, MemberPage } from './pages';
 
 const onUpdate = () => window.scrollTo(0, 0);
 
