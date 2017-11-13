97a8df565b99d05853675a121ff392823f6031d7
Harry <hfitz83@gmail.com>
2017-11-13 20:52:05 +0000

    temp for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const prod = require('./webpack.config.prod');
 const dev = require('./webpack.config.dev');
 
-if (process.env.NODE_ENV !== 'production') {
-  module.exports = dev;
-} else {
-  module.exports = prod;
-}
+// if (process.env.NODE_ENV !== 'production') {
+module.exports = dev;
+// } else {
+// module.exports = prod;
+// }
