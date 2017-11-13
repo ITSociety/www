0e771cad6aaa93f5ea92405e74bce869b7f4917c
Harry <hfitz83@gmail.com>
2017-11-13 20:49:52 +0000

    member page possibly done

--- src/client/pages/member.jsx
+++ src/client/pages/member.jsx
@@ -6,11 +6,21 @@ import Typography from 'material-ui/Typography';
 import Card, { CardContent, CardMedia } from 'material-ui/Card';
 import Icon from 'material-ui/Icon';
 
-const generatePage = member => {
-  <div className='member'>
-    
-};
-
+const generatePage = member => (
+  <div className="member">
+    <Card className="member-card">
+      <CardMedia image={member.image} className="member-image" />
+      <CardContent className="member-content">
+        <div className="member-headline">
+          <Typography type="display1">{member.name}</Typography>
+          <Typography type="headline" component="h2" color="secondary">
+            {member.role}
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+);
 
 export default class MemberPage extends Component {
   constructor(props) {
@@ -22,8 +32,9 @@ export default class MemberPage extends Component {
   async componentWillMount() {
     const url = `/api/contentful/committee/${this.id}`;
     const memberInfo = await getEndpoint(url);
+    console.log(memberInfo);
     const children = generatePage(memberInfo);
-    // this.setState({ children });
+    this.setState({ children });
   }
 
   render() {
